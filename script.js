35b36ec7686f5fe93ae62a51443af857db653f79
Sachin <160299457+sachinnn7988@users.noreply.github.com>
2025-07-07 10:52:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function () {
+        
+            $('.menu-toggle').click(function () {
+                console.log('you click on p')
+                $('.navbars').toggle();
+
+            });
+
+        })
+
